aa8d1f4070db0d26b034f4f35034c38796a9aa7e
fiona <lei.xw.li@hotmail.com>
2023-11-23 22:15:04 -0800

    before wrap up server

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,28 +10,6 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// Sign up
-// router.post(
-//     '/',
-//     async (req, res) => {
-//       const { email, password, username } = req.body;
-//       const hashedPassword = bcrypt.hashSync(password);
-//       const user = await User.create({ email, username, hashedPassword });
-
-//       const safeUser = {
-//         id: user.id,
-//         email: user.email,
-//         username: user.username,
-//       };
-
-//       await setTokenCookie(res, safeUser);
-
-//       return res.json({
-//         user: safeUser
-//       });
-//     }
-//   );
-
   const validateSignup = [
     check('email')
       .exists({ checkFalsy: true })
